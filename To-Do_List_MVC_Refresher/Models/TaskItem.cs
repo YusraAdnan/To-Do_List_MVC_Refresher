c2695dd94ab3ca9bbdcddf22ab5291a92c3a9123
YusraAdnan <101713869+YusraAdnan@users.noreply.github.com>
2026-08-06 11:05:39 +0200

    Add project files.

---
+++ To-Do_List_MVC_Refresher/Models/TaskItem.cs
@@ -0,0 +1,11 @@
+﻿namespace To_Do_List_MVC_Refresher.Models
+{
+    public partial class TaskItem
+    {
+        public Guid Id { get; set; }
+
+        public string Title { get; set; } = null!;
+
+        public bool IsComplete { get; set; }
+    }
+}
